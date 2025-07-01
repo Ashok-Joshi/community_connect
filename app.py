60d198208cca9817e756a29490d80ad0870091b8
Ashok_Joshi <ashokjoshibng@gmail.com>
2025-07-01 14:44:58 +0530

    Initial commit: Flask login/signup setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html') # To render index.html as my default page
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
